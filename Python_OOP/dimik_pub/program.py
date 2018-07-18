2b5404191d6e57113db19c163f112024ecf48b41
Md. Nur Alam Siddique <nur.alam.jewel72@gmail.com>
2018-07-18 12:52:57 +0600

    update

--- Python_OOP/dimik_pub/program.py
+++
@@ -1,7 +0,0 @@
-import info_from_web as ifw
-import info_from_web2 as ifw2
-
-sub_dir_name=ifw2.sub_dir_name()
-
-for item in sub_dir_name:
-    ifw.create_dir(item)
--- Python_OOP/info_from_web_full_project.py
+++ Python_OOP/info_from_web_full_project.py
@@ -1,73 +1,72 @@
-'''import os
-
-dir_name='dimik_pub'
-os.mkdir(dir_name)'''
-
-
-
-
-
-import re
 import os
 import requests
+import re
 import sys
 
 def create_dir(name):
+    print('creating directory',name)
     try:
         os.mkdir(name)
-    except FileExistsError:
-        print(name,'already exist')
+    except Exception as e:
+        print(e)
 
-def write_book_info(book_name,url,file_name):
-    print('your book info is writting')
-    with open(file_name,'w',encoding=response.encoding) as f:
-        f.write(name+'\n')
-        f.write(url)
-
-def downlaod_image(img_url,file_name):
-    print('downlaoding image')
+def down_image(img_url,file_name):
+    print('downloading image!')
     r=requests.get(img_url)
 
     with open(file_name,'wb') as f:
         f.write(r.content)
 
-def get_dir_name(reg_ex,url):
-    result=re.findall(reg_ex,url)
+def save_book_info(book_name,book_url,file_name):
+    print('saving '+book_name+' info')
+    with open(file_name,'w',encoding='utf-8') as f:
+        f.write(book_name)
+        f.write('\n')
+        f.write(book_url)
+
+def get_dir_name(reg_exp,url):
+    result=re.findall(reg_exp,url)
     dir_name='_'.join(result[0])
     return dir_name
 
 def process():
+    #create home directory
     create_dir(main_dir)
 
 
 
-main_dir='dimik_pub'
-
-url='http://dimik.pub'
-response=requests.get(url)
-if response.ok is False:
-    sys.exit('could not get response from server')
-page_content=response.text
-
-reg_ex=re.compile(r'<div class="book-cover">\s*<a href="(.*?)">\s*<img src="(.*?)".*?<h2 class="sd-title">\s*<a.*?>(.*?)<',re.S)
-result=re.findall(reg_ex,page_content)
-
-dir_reg_ex=re.compile(r'book/(\d+)/(\w+)-(\w+)-')
-
-for item in result:
-    name=item[2]
-    url=item[0]
-    img_url=item[1]
-
-    dir_name=main_dir+'_'+get_dir_name(dir_reg_ex,url)
-    create_dir(dir_name)
-
-    file_name=dir_name+'_'+'info.txt'
-    write_book_info(name,url,file_name)
-
-    img_file_name=dir_name+'_'+'image.jpg'
-    downlaod_image(img_url,img_file_name)
-
-
 if __name__=='__main__':
+
+    main_dir='dimik_pub'
+
     process()
+
+    url='http://dimik.pub/'
+
+    res=requests.get(url)
+
+    if res.ok is False:
+        sys.exit('couldn\'t get response from server')
+
+    page_cont=res.text
+    new_page_cont=page_cont.replace('\n',' ')
+
+    pat=re.compile(r'<div class="book-cover">\s*<a.+?"(.+?)">.+?"(.+?)">.+?<h2.+?<a.+?>(.+?)<')
+
+    result=re.findall(pat,new_page_cont)
+
+    dir_pat=re.compile(r'book/(\d+)/(\w+)-(\w+)-')
+
+    for item in result:
+        name=item[2]
+        url=item[0]
+        img_url=item[1]
+
+        dir_name=main_dir+'/'+get_dir_name(dir_pat,url)
+        create_dir(dir_name)
+
+        file_name=dir_name+'/info.txt'
+        save_book_info(name,url,file_name)
+
+        img_file_name=dir_name+'/image.png'
+        down_image(img_url,img_file_name)
